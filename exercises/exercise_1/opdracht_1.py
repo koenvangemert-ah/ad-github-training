88349a95a422c5804236aa6d611fa386aa59b1f6
Koen van Gemert <pnl12a8d@Emea.Royalahold.net>
2023-10-11 10:47:10 +0200

    Eerste functie

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -1,2 +1,12 @@
 def likes(team: list) -> str:
-    return True
+    """
+    Function returns who liked your post on Facebook
+    :param: List om teammembers (lst)
+    :return: A statement of who liked your post (str)
+    """
+    if len(team) == 0:
+        return "no one likes this"
+    elif len(team) == 1:
+        return team[0] + " likes this"
+    else:
+        return team[0] + " en " + team[1] + " like this"
